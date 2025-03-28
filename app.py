da439e5cf104cf7b8fb041ed3d77a75f8504bfbd
FacuFre <100240157+FacuFre@users.noreply.github.com>
2025-03-27 23:40:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
-from flask import Flask
 from pyhomebroker import HomeBroker
 import requests
 import os
-
-app = Flask(__name__)
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_API_KEY = os.getenv("SUPABASE_API_KEY")
@@ -23,15 +20,11 @@ def guardar_en_supabase(symbol, description):
     response = requests.post(url, headers=headers, json=data)
     print(f"Insertando {symbol}: {response.status_code}")
 
-@app.route("/")
-def home():
+def main():
     hb = HomeBroker()
     instruments = hb.get_instruments()
 
-    for inst in instruments[:10]:  # ejemplo con los primeros 10
+    for inst in instruments[:10]:
         guardar_en_supabase(inst["symbol"], inst["description"])
 
-    return "Datos enviados a Supabase"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+if __name__ == "__main
